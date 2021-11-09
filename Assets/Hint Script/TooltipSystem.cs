661cc4896b11bc836f8cbf92575f6820e2a948fa
zhudson26 <zhudson26@ymail.com>
2021-11-08 21:17:15 -0500

    Added hint system

---
+++ Assets/Hint Script/TooltipSystem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipSystem : MonoBehaviour
+{
+    private static TooltipSystem current;
+
+    public Tooltip tooltip;
+
+    public void Awake()
+    {
+        current = this;
+        Hide();
+    }
+
+    public static void Show(string content, string header = "")
+    {
+        current.tooltip.gameObject.SetActive(true);
+        current.tooltip.SetText(content, header);
+    }
+
+    public static void Hide()
+    {
+        current.tooltip.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Hint Script/TooltipTrigger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    private static LTDescr delay;
+    public string header;
+
+    [Multiline()]
+    public string content;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        delay = LeanTween.delayedCall(.7f, () =>
+        {
+            TooltipSystem.Show(content, header);
+        });
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        LeanTween.cancel(delay.uniqueId);
+        TooltipSystem.Hide();
+    }
+
+    //Hides the tooltip when clicking, otherwise it continues to show when opening menus since OnPointerExit won't trigger when adding popups
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        TooltipSystem.Hide();
+    }
+}
+
